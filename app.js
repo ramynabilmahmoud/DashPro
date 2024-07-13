9b46a803f1bd3ab84cb3a18b75b4575cbe3a5158
ramynabilmahmoud <ramynabil680@gmail.com>
2024-07-13 15:05:59 +0300

    connecto to mongoDB

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const mongoose = require("mongoose");
 
 app.get("/", (req, res) => {
   res.sendFile("./views/home.html", { root: __dirname });
 });
 
-app.listen(port, () => {
-  console.log(`http://localhost:${port}`);
-});
+mongoose
+  .connect(
+    "mongodb+srv://ramynabilmahmoud:8UqIs8er3PQkbbTF@cluster0.afaajs1.mongodb.net/all-data?retryWrites=true&w=majority&appName=Cluster0"
+  )
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`http://localhost:${port}`);
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
