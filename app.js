35945d26230eeb89ddc701d87a3a8b6e8bee5a27
ramynabilmahmoud <ramynabil680@gmail.com>
2024-07-13 22:11:30 +0300

    style and images

--- app.js
+++ app.js
@@ -2,23 +2,26 @@ const express = require("express");
 const app = express();
 const port = 3000;
 const mongoose = require("mongoose");
-const Mydata = require("./models/mydataSchema");
+const myData = require("./models/schema");
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
 app.get("/", (req, res) => {
-  Mydata.find()
-    .then((result) => {
-      res.render("home", { title: "Home Page", data: result });
-    })
-    .catch((err) => {
-      console.log(err);
-    });
+  res.render("index");
 });
 
-app.get("/index.html", (req, res) => {
-  res.send("<h1>Sent To DB </h1>");
+app.get("/user/add.html", (req, res) => {
+  res.render("user/add");
 });
 
+app.get("/user/view.html", (req, res) => {
+  res.render("user/view");
+});
+
+app.get("/user/edit.html", (req, res) => {
+  res.render("user/edit");
+});
 mongoose
   .connect(
     "mongodb+srv://ramynabilmahmoud:8UqIs8er3PQkbbTF@cluster0.afaajs1.mongodb.net/all-data?retryWrites=true&w=majority&appName=Cluster0"
@@ -31,16 +34,3 @@ mongoose
   .catch((err) => {
     console.log(err);
   });
-
-app.post("/save", (req, res) => {
-  console.log(req.body);
-  const data = new Mydata(req.body);
-  data
-    .save()
-    .then(() => {
-      res.redirect("/index.html");
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-});
