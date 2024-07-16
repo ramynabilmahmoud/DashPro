4e804b38d6392da256928ceb7fa95e9d88a8aee6
ramynabilmahmoud <ramynabil680@gmail.com>
2024-07-16 21:04:39 +0300

    ..

---
+++ controllers/usersController.js
@@ -0,0 +1,302 @@
+const User = require("../models/customerSchema");
+var moment = require("moment");
+var country_list = [
+  "Afghanistan",
+  "Albania",
+  "Algeria",
+  "Andorra",
+  "Angola",
+  "Anguilla",
+  "Antigua & Barbuda",
+  "Argentina",
+  "Armenia",
+  "Aruba",
+  "Australia",
+  "Austria",
+  "Azerbaijan",
+  "Bahamas",
+  "Bahrain",
+  "Bangladesh",
+  "Barbados",
+  "Belarus",
+  "Belgium",
+  "Belize",
+  "Benin",
+  "Bermuda",
+  "Bhutan",
+  "Bolivia",
+  "Bosnia & Herzegovina",
+  "Botswana",
+  "Brazil",
+  "British Virgin Islands",
+  "Brunei",
+  "Bulgaria",
+  "Burkina Faso",
+  "Burundi",
+  "Cambodia",
+  "Cameroon",
+  "Cape Verde",
+  "Cayman Islands",
+  "Chad",
+  "Chile",
+  "China",
+  "Colombia",
+  "Congo",
+  "Cook Islands",
+  "Costa Rica",
+  "Cote D Ivoire",
+  "Croatia",
+  "Cruise Ship",
+  "Cuba",
+  "Cyprus",
+  "Czech Republic",
+  "Denmark",
+  "Djibouti",
+  "Dominica",
+  "Dominican Republic",
+  "Ecuador",
+  "Egypt",
+  "El Salvador",
+  "Equatorial Guinea",
+  "Estonia",
+  "Ethiopia",
+  "Falkland Islands",
+  "Faroe Islands",
+  "Fiji",
+  "Finland",
+  "France",
+  "French Polynesia",
+  "French West Indies",
+  "Gabon",
+  "Gambia",
+  "Georgia",
+  "Germany",
+  "Ghana",
+  "Gibraltar",
+  "Greece",
+  "Greenland",
+  "Grenada",
+  "Guam",
+  "Guatemala",
+  "Guernsey",
+  "Guinea",
+  "Guinea Bissau",
+  "Guyana",
+  "Haiti",
+  "Honduras",
+  "Hong Kong",
+  "Hungary",
+  "Iceland",
+  "India",
+  "Indonesia",
+  "Iran",
+  "Iraq",
+  "Ireland",
+  "Isle of Man",
+  "Israel",
+  "Italy",
+  "Jamaica",
+  "Japan",
+  "Jersey",
+  "Jordan",
+  "Kazakhstan",
+  "Kenya",
+  "Kuwait",
+  "Kyrgyz Republic",
+  "Laos",
+  "Latvia",
+  "Lebanon",
+  "Lesotho",
+  "Liberia",
+  "Libya",
+  "Liechtenstein",
+  "Lithuania",
+  "Luxembourg",
+  "Macau",
+  "Macedonia",
+  "Madagascar",
+  "Malawi",
+  "Malaysia",
+  "Maldives",
+  "Mali",
+  "Malta",
+  "Mauritania",
+  "Mauritius",
+  "Mexico",
+  "Moldova",
+  "Monaco",
+  "Mongolia",
+  "Montenegro",
+  "Montserrat",
+  "Morocco",
+  "Mozambique",
+  "Namibia",
+  "Nepal",
+  "Netherlands",
+  "Netherlands Antilles",
+  "New Caledonia",
+  "New Zealand",
+  "Nicaragua",
+  "Niger",
+  "Nigeria",
+  "Norway",
+  "Oman",
+  "Pakistan",
+  "Palestine",
+  "Panama",
+  "Papua New Guinea",
+  "Paraguay",
+  "Peru",
+  "Philippines",
+  "Poland",
+  "Portugal",
+  "Puerto Rico",
+  "Qatar",
+  "Reunion",
+  "Romania",
+  "Russia",
+  "Rwanda",
+  "Saint Pierre & Miquelon",
+  "Samoa",
+  "San Marino",
+  "Satellite",
+  "Saudi Arabia",
+  "Senegal",
+  "Serbia",
+  "Seychelles",
+  "Sierra Leone",
+  "Singapore",
+  "Slovakia",
+  "Slovenia",
+  "South Africa",
+  "South Korea",
+  "Spain",
+  "Sri Lanka",
+  "St Kitts & Nevis",
+  "St Lucia",
+  "St Vincent",
+  "St. Lucia",
+  "Sudan",
+  "Suriname",
+  "Swaziland",
+  "Sweden",
+  "Switzerland",
+  "Syria",
+  "Taiwan",
+  "Tajikistan",
+  "Tanzania",
+  "Thailand",
+  "Timor L'Este",
+  "Togo",
+  "Tonga",
+  "Trinidad & Tobago",
+  "Tunisia",
+  "Turkey",
+  "Turkmenistan",
+  "Turks & Caicos",
+  "Uganda",
+  "Ukraine",
+  "United Arab Emirates",
+  "United Kingdom",
+  "Uruguay",
+  "Uzbekistan",
+  "Venezuela",
+  "Vietnam",
+  "Virgin Islands (US)",
+  "Yemen",
+  "Zambia",
+  "Zimbabwe",
+];
+const user_index_get = (req, res) => {
+  // result ==> array of objects
+  console.log("--------------------------------------------");
+  User.find()
+    .then((result) => {
+      res.render("index", { arr: result, moment: moment });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+const user_edit_get = (req, res) => {
+  User.findById(req.params.id)
+    .then((result) => {
+      res.render("user/edit", { obj: result, moment: moment });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+const user_view_get = (req, res) => {
+  // result ==> object
+  User.findById(req.params.id)
+    .then((result) => {
+      res.render("user/view", { obj: result, moment: moment });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+const user_search_post = (req, res) => {
+  console.log("*******************************");
+
+  const searchText = req.body.searchText.trim();
+
+  User.find({ $or: [{ fireName: searchText }, { lastName: searchText }] })
+    .then((result) => {
+      console.log(result);
+      res.render("user/search", { arr: result, moment: moment });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+const user_delete = (req, res) => {
+  User.deleteOne({ _id: req.params.id })
+    .then((result) => {
+      res.redirect("/");
+      console.log(result);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+const user_put = (req, res) => {
+  User.updateOne({ _id: req.params.id }, req.body)
+    .then((result) => {
+      res.redirect("/");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+const user_add_get = (req, res) => {
+  res.render("user/add");
+};
+
+const user_post = (req, res) => {
+  User.create(req.body)
+    .then(() => {
+      res.redirect("/");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+module.exports = {
+  user_index_get,
+  user_edit_get,
+  user_view_get,
+  user_search_post,
+  user_delete,
+  user_put,
+  user_add_get,
+  user_post,
+};
--- routes/allRoutes.js
+++ routes/allRoutes.js
@@ -2,53 +2,14 @@ const express = require("express");
 const router = express.Router();
 const userSchema = require("../models/customerSchema");
 const moment = require("moment");
-
+const userController = require("../controllers/usersController");
 // GET Request
-router.get("/", (req, res) => {
-  userSchema
-    .find()
-    .then((result) => {
-      res.render("index", { user: result, moment: moment });
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-});
+router.get("/", userController.user_index_get);
 
-router.get("/view/:id", (req, res) => {
-  userSchema
-    .findById(req.params.id)
-    .then((result) => {
-      res.render("user/view", { userDetails: result, moment: moment });
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-});
+router.get("/view/:id", userController.user_view_get);
 
-router.post("/search", (req, res) => {
-  const searchText = req.body.searchText.trim().toLowerCase();
-  userSchema
-    .find({
-      $or: [{ firstName: searchText }, { lastName: searchText }],
-    })
-    .then((result) => {
-      res.render("user/search", { user: result, moment: moment });
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-});
+router.post("/search", userController.user_search_post);
 
-router.delete("/delete/:id", (req, res) => {
-  userSchema
-    .findByIdAndDelete(req.params.id)
-    .then((result) => {
-      res.redirect("/");
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-});
+router.delete("/delete/:id", userController.user_delete);
 
 module.exports = router;
